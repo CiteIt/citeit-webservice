321feea5a01cda47ca71086e2dffa52c848387b4
Tim Langeman <timlangeman@gmail.com>
2018-07-04 09:35:58 -0400

    Rename CiteIt.py -> app.py
      * Rename CiteIt.py -> app.py
      * settings-default.py: increment version number

--- settings-default.py
+++ settings-default.py
@@ -5,7 +5,7 @@ AMAZON_SECRET_KEY = 'alksdfj;2452lkjr;ajtsaljgfslakjfgassgf'
 AMAZON_S3_BUCKET = 'read.citeit.net'
 AMAZON_S3_ENDPOINT = 's3.amazonaws.com'
 
-VERSION_NUM = '0.03'  # used to version the sha256 hash filename
+VERSION_NUM = '0.30'  # used to version the sha256 hash filename
 
 JSON_FILE_PATH = '/Users/timlangeman/Documents/citeit/json/'
 SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost/database_name'
