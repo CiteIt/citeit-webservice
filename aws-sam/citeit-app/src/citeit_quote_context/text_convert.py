f5822fdb0ddba78bd861cd7a8a0741b886caa106
Matt Langeman <matt@langeman.net>
2018-11-15 16:49:47 -0500

    copy of base citeit project into new folder for aws-sam

---
+++ aws-sam/citeit-app/src/citeit_quote_context/text_convert.py
@@ -0,0 +1,51 @@
+# Copyright (C) 2015-2018 Tim Langeman and contributors
+# <see AUTHORS.txt file>
+#
+# This library is part of the CiteIt project:
+# http://www.citeit.net/
+
+# The code for this server library is released under the MIT License:
+# http://www.opensource.org/licenses/mit-license
+
+from bs4 import BeautifulSoup
+import re
+
+__author__ = 'Tim Langeman'
+__email__ = "timlangeman@gmail.com"
+__copyright__ = "Copyright (C) 2015-2018 Tim Langeman"
+__license__ = "MIT"
+__version__ = "0.3"
+
+
+from bs4 import BeautifulSoup
+
+
+class TextConvert:
+
+    def __init__(self, str, url=None):
+        self.str = str  # page's html, containing potential link, meta tags
+
+    def escape(self):
+        """ Remove the following characters from a string."""
+
+        txt = self.str
+
+        replace_text = [' ', '\n', 'â€™', ',', '.', '-', '–','-','-', '—' ':', '/', '!', '`', '~', '^', "’"
+            , '&nbsp', '\xa0', '&#8217;'
+            , '&#169;', '&copy;', '&#174;'
+            , '&reg;', '&#8364;', '&euro;', '&#8482;', '&trade;'
+            , '&lsquo;', '&rsquo;', '&sbquo;', '&ldquo;', '&rdquo;', '&bdquo;'
+            , '&#34;', '&quot;', '&#38;', '&amp;', '&#39;', '&#163;', '&pound;'
+            , '&#165;', '&yen;', '&#168;', '&uml;', '&die;', '&#169;', '&copy;'
+            , '\u201c', '“', '”', "‘", "’", '’'
+            , '&#8217;', '&#8230;',
+        ]
+
+        for t in replace_text:
+            txt = txt.replace(t, '')
+
+        return txt
+
+def html_to_text(html_str):
+    soup = BeautifulSoup(html_str, 'html.parser')
+    return soup.get_text()
---
+++ aws-sam/citeit-app/src/settings-default.py
@@ -0,0 +1,11 @@
+
+# AMAZON S3 Login information
+AMAZON_ACCESS_KEY = 'ABCDEFGH123456789'
+AMAZON_SECRET_KEY = 'alksdfj;2452lkjr;ajtsaljgfslakjfgassgf'
+AMAZON_S3_BUCKET = 'read.citeit.net'
+AMAZON_S3_ENDPOINT = 's3.amazonaws.com'
+
+VERSION_NUM = '0.30'  # used to version the sha256 hash filename
+
+JSON_FILE_PATH = '/Users/timlangeman/Documents/citeit/json/'
+SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost/database_name'
