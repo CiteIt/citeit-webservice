74e6796efb87afe621072adf0887372dee5765d7
Tim Langeman <timlangeman@gmail.com>
2019-10-13 14:13:34 -0400

    Update Quote: add extra meta data fields
      Update Quote: add extra meta data fields

--- aws-sam/citeit-app/src/lib/citeit_quote_context/quote.py
+++ aws-sam/citeit-app/src/lib/citeit_quote_context/quote.py
@@ -30,46 +30,95 @@ __version__ = "0.3"
 class Quote:
     """Looks up quote from cited url and returns the surrounding context
 
-    * Calculates hash of: canonical urls from: citing_url| cited_url|quote
-    * Computes text version of html
-    * Calculates quote context using: QuoteContext class
+      * Calculates hash of: canonical urls from: citing_url| cited_url|quote
+      * Computes text version of html
+      * Calculates quote context using: QuoteContext class
         which uses google_diff_match_patch (levenshtein) algorithm
-    * Returns: dictionary: data()
+      * Returns: dictionary: data()
 
-    Usage: Quote (
-        citing_quote="one does not live by bread alone, "
-        "but by every word that comes from the mouth of the Lord",
-        citing_url='http://www.citeit.net/demo/',
-        cited_url='https://www.biblegateway.com/passage/?search=Deuteronomy+8&amp;version=NRSV'
-    )
+        Usage: Quote (
+          citing_quote="one does not live by bread alone, "
+          "but by every word that comes from the mouth of the Lord",
+          citing_url='http://www.citeit.net/demo/',
+          cited_url='https://www.biblegateway.com/passage/?search=Deuteronomy+8&amp;version=NRSV'
+        )
     """
 
     def __init__(
         self,
-        citing_quote_input,       # excerpt from citing document
-        citing_url_input,         # url of the document that is doing the quoting
-        cited_url_input,          # url of document that is being quoted
-        citing_text_input='',     # optional: text from citing document
-        citing_raw_input='',      # optional: raw content of citing document
-        text_output=True,   # output computed text version of url's html
-        raw_output=True,    # output full html/pdf source of cited url
-        prior_quote_context_length=500, # length of excerpt before quote
-        after_quote_context_length=500, # length of excerpt after quote
-        starting_location_guess=None    # guess used by google diff_match_patch
+        citing_quote_input,     # excerpt from citing document
+        citing_url_input,       # url of the document that is doing the quoting
+        cited_url_input,        # url of document that is being quote
+        citing_text_input='',   # optional: text from citing document
+        citing_raw_input='',    # optional: raw content of citing document
+        cited_audio_url='',     # optional: audio/podcast URL
+        cited_video_url='',     # optional: video url
+        cited_audio_start_time='',     # optional: number of seconds
+        cited_video_start_time='',     # optional: number of seconds
+
+        cited_transcript_url='',   # optional: transcript url
+        citing_tags='',            # optional: custom tags
+        cited_microformat_url='',  # optional: http://microformats.org/wiki/microformats2
+        cited_title='',            # optional: title: "Patient Zero"
+        cited_authors_entities='',    # optional: collective entity: "Radio Lab"
+        cited_authors_entities_urls = '',  # optional: https://www.npr.org/series/121566675/radiolab, url2
+        cited_authors_names='',       # optional: author names: comma separated
+        cited_authors_urls='',             # optional: author homepage
+        cited_authors_twitter='',          # optional: twitter homepage
+        cited_authors_wikipedia_urls ='',  # optional: wikipedia url
+        cited_author_wikipedia_url='',     # optional: author Wikipedia URL
+        cited_series = '',         # optional: Series name
+        cited_series_url = '',      # optional: Series urls=
+        cited_series_youtube = '',  # optional: Series YouTube URL
+        cited_event_date='',               # optional: date written/said
+        cited_event_location='',  # optional: location name: Washington, DC (USA)
+        cited_event_lattitue='',        # optional: lattitude: 40.7128
+        cited_event_longitude='',       # optional: longitude: 74.0060
+        text_output=True,           # output computed text version of url's html
+        raw_output=True,            # output full html/pdf source of cited url
+        prior_quote_context_length=500,  # length of excerpt before quote
+        after_quote_context_length=500,  # length of excerpt after quote
+        starting_location_guess=None     # guess used by google diff_match_patch
     ):
-        self.start_time = time.time()   # measure elapsed time
+
+
+    'cited-work-wikipedia-url',
+
+    self.start_time = time.time()    # measure elapsed time
         self.citing_quote_input = citing_quote_input
         self.citing_url_input = citing_url_input
         self.cited_url_input = cited_url_input
         self.citing_text_input = citing_text_input
         self.citing_raw_input = citing_raw_input
+
+        self.cited_audio_url = cited_audio_url
+        self.cited_video_url = cited_video_url
+        self.cited_transcript_url = cited_transcript_url
+        self.citing_tags = citing_tags
+        self.cited_microformat_url = cited_microformat_url
+        self.cited_title = cited_title
+        self.cited_authors_entities = cited_authors_entities
+        self.cited_authors_entities_urls = cited_authors_entities_urls
+        self.cited_authors_names = cited_authors_names
+        self.cited_authors_urls = cited_authors_urls
+
+
+        self.cited_authors-twitter = '',
+        self.cited_authors_wikipedia_urls =
+
+self.cited_author_wikipedia_url = cited_author_wikipedia_url
+        self.cited_event_date = cited_event_date
+        self.cited_event_location = cited_event_location
+        self.cited_event_lattitue = cited_event_lattitue
+        self.cited_event_longitude = cited_event_longitude
+
         self.text_output = text_output
         self.raw_output = raw_output
         self.prior_quote_context_length = prior_quote_context_length
         self.after_quote_context_length = after_quote_context_length
         self.starting_location_guess = starting_location_guess
 
-    ######################## Citing Document ############################
+  ######################## Citing Document ############################
 
     def citing_quote(self):
         return html_to_text(self.citing_quote_input)
@@ -114,7 +163,6 @@ class Quote:
 
         citing = Canonical_URL(html, url)
         return citing.citeit_url()
-        #return self.citing_url()
 
 
     ########################## Cited Document ##########################
@@ -165,20 +213,16 @@ class Quote:
             throw off the hash
         """
 
-        # soup = BeautifulSoup(self.citing_quote, "html.parser")
-        # citing_quote = soup.get_text()
-
         citing_quote = self.citing_quote()
         citing_quote = TextConvert(citing_quote).escape()
         citing_url = self.citing_url_canonical()
         cited_url = self.cited_url()
 
-
         return ''.join([
-            citing_quote, '|',
-                    url_without_protocol(citing_url), '|',
-                    url_without_protocol(cited_url)  # future: replace with: self.cited_url_canonical ?
-                ])
+          citing_quote, '|',
+            url_without_protocol(citing_url), '|',
+            url_without_protocol(cited_url)  # future: replace with: self.cited_url_canonical ?
+        ])
 
     def hash(self):
         """
@@ -209,17 +253,27 @@ class Quote:
         """
 
         data_dict = {
-            'sha256': self.hash(),
-            'citing_quote': self.citing_quote(),
-            'citing_url': self.citing_url(),  #  may be different from canonical
-            'cited_url': self.cited_url(),    #  may be different from canonical
-            'citing_url_canonical': self.citing_url_canonical(),
-            'cited_url_canonical': self.cited_url_canonical(),
+          'sha256': self.hash(),
+          'citing_quote': self.citing_quote(),
+          'citing_url': self.citing_url(),  #  may be different from canonical
+          'cited_url': self.cited_url(),    #  may be different from canonical
+          'citing_url_canonical': self.citing_url_canonical(),
+          'cited_url_canonical': self.cited_url_canonical(),
+
+          'cited_audio_url': self.cited_audio_url,
+          'cited_video_url': self.cited_video_url,
+          'cited_transcript_url': self.cited_transcript_url,
+          'citing_tags': self.citing_tags,
+          'cited_authors_names': self.cited_authors_names,
+          'cited_authors_urls': self.cited_authors_urls,
+          'cited_author_wikipedia_url': self.cited_author_wikipedia_url,
+          'cited_event_date': self.cited_event_date,
+          'cited_event_location': self.cited_event_location,
+          'cited_event_lattitue': self.cited_event_lattitue,
+          'cited_event_longitude': self.cited_event_longitude
         }
 
         ######### LEFT OFF ##########
-
-
 
 
         # Set text and raw values equal to user-supplied values, or look up
@@ -275,7 +329,4 @@ class Quote:
             for excluded_field in excluded_fields:
                 data_dict.pop(excluded_field, None)
 
-        """  ##### LEFT OFF #####
-        """
         return data_dict
-
