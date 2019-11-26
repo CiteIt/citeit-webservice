4be0ea389672d9be5e73e374bbd85e1939f4b174
Tim Langeman <timlangeman@gmail.com>
2019-11-25 21:33:01 -0500

    Update Test of Hash function
      * pass javascript hashkey to test if it matches python result
      * add loop to report testing results

--- app/test.py
+++ app/test.py
@@ -18,20 +18,32 @@ __copyright__ = "Copyright (C) 2015-2019 Tim Langeman"
 __license__ = "MIT"
 __version__ = "0.3"
 
+debug = False
+
 
 hash_javascript = "dbff36fd9d7f846834a8ffdf62bdcf4b45a4bace30912c22d1018718a70f8d09"
 citing_quote = "<p>Greenspan maximised a form of power that is invaluable to experts. Because journalists admired him, it was dangerous for politicians to pick a fight with the Fed: in any public dispute, the newspaper columnists and talking heads would take Greenspan’s side of the argument. As a result, the long tradition of Fed-bashing ceased almost completely. Every Washington insider understood that Greenspan was too powerful to touch.</p>"
 citing_url = "http://demo.citeit.net/index.php/2016/10/22/the-cult-of-the-expert/"
 cited_url = "https://www.theguardian.com/business/2016/oct/20/alan-greenspan-cult-of-expert-and-how-it-collapsed"
+citing_hashkey = "GreenspanmaximisedaformofpowerthatisinvaluabletoexpertsBecausejournalistsadmiredhimitwasdangerousforpoliticianstopickafightwiththeFedinanypublicdisputethenewspapercolumnistsandtalkingheadswouldtakeGreenspanssideoftheargumentAsaresultthelongtraditionofFedbashingceasedalmostcompletelyEveryWashingtoninsiderunderstoodthatGreenspanwastoopowerfultotouch|http://demo.citeit.net/index.php/2016/10/22/the-cult-of-the-expert/|https://www.theguardian.com/business/2016/oct/20/alan-greenspan-cult-of-expert-and-how-it-collapsed"
 
 q = QuoteHashTest(
     hash_javascript,
     citing_quote,   # excerpt from citing document
     citing_url,     # url of the document that is doing the quoting
     cited_url,      # url of document that is being quoted
+    citing_hashkey
 )
-print("URL:       " + q.citing_url)
-#print("Quote:     " + q.citing_quote)
-print("JS Hash:   " + q.hash_javascript)
-print("Computed:  " + str(q.hash_computed()))
-print("Match:     " + str(q.hash_match()))
+
+if debug:
+    print("URL:         " + str(q.citing_url))
+    print("Quote:       " + str(q.citing_quote))
+
+    print("Citing Hash:  " + q.hash_citing())
+    print("Cited Hash:   " + str(q.hash_cited()))
+
+url_results = {}
+url_results[citing_url] = q.hash_match()
+
+for url, url_match in url_results.items():
+    print(url_match, url)
