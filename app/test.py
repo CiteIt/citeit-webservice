c55e753995d899fc770079de9ca43442fac154c6
Tim Langeman <timlangeman@gmail.com>
2019-11-26 23:22:52 -0500

    Improve Test for QuoteHastTest
      * import Google spreadsheet data from CSV
      * loop through data and test whether the
        javascript hash matches python hash

--- app/test.py
+++ app/test.py
@@ -10,6 +10,8 @@
 # http://www.opensource.org/licenses/mit-license
 
 from lib.citeit_quote_context.tests.quote_hash_match import QuoteHashTest
+import os
+import csv
 
 
 __author__ = 'Tim Langeman'
@@ -20,30 +22,35 @@ __version__ = "0.3"
 
 debug = False
 
-
-hash_javascript = "dbff36fd9d7f846834a8ffdf62bdcf4b45a4bace30912c22d1018718a70f8d09"
-citing_quote = "<p>Greenspan maximised a form of power that is invaluable to experts. Because journalists admired him, it was dangerous for politicians to pick a fight with the Fed: in any public dispute, the newspaper columnists and talking heads would take Greenspan’s side of the argument. As a result, the long tradition of Fed-bashing ceased almost completely. Every Washington insider understood that Greenspan was too powerful to touch.</p>"
-citing_url = "http://demo.citeit.net/index.php/2016/10/22/the-cult-of-the-expert/"
-cited_url = "https://www.theguardian.com/business/2016/oct/20/alan-greenspan-cult-of-expert-and-how-it-collapsed"
-citing_hashkey = "GreenspanmaximisedaformofpowerthatisinvaluabletoexpertsBecausejournalistsadmiredhimitwasdangerousforpoliticianstopickafightwiththeFedinanypublicdisputethenewspapercolumnistsandtalkingheadswouldtakeGreenspanssideoftheargumentAsaresultthelongtraditionofFedbashingceasedalmostcompletelyEveryWashingtoninsiderunderstoodthatGreenspanwastoopowerfultotouch|http://demo.citeit.net/index.php/2016/10/22/the-cult-of-the-expert/|https://www.theguardian.com/business/2016/oct/20/alan-greenspan-cult-of-expert-and-how-it-collapsed"
-
-q = QuoteHashTest(
-    hash_javascript,
-    citing_quote,   # excerpt from citing document
-    citing_url,     # url of the document that is doing the quoting
-    cited_url,      # url of document that is being quoted
-    citing_hashkey
-)
-
-if debug:
-    print("URL:         " + str(q.citing_url))
-    print("Quote:       " + str(q.citing_quote))
-
-    print("Citing Hash:  " + q.hash_citing())
-    print("Cited Hash:   " + str(q.hash_cited()))
-
+filepath = "test_data/hashkeyTests - version3.csv"
+column_names = ['CitingURL', 'Quote', 'CitedURL', 'JavascriptHash', 'JavascriptHashKey']
 url_results = {}
-url_results[citing_url] = q.hash_match()
+
+file = open(os.path.join(filepath), "r")
+reader = csv.DictReader(file, delimiter=',')
+
+for row in reader:
+    for column_name in column_names:
+        if debug:
+            print(column_name, " : " , row[column_name])
+
+    q = QuoteHashTest(
+        row['JavascriptHash'],
+        row['Quote'],   # excerpt from citing document
+        row['CitingURL'],     # url of the document that is doing the quoting
+        row['CitedURL'],      # url of document that is being quoted
+        row['JavascriptHashKey']
+    )
+
+    if debug:
+        print("URL:         " + str(q.citing_url))
+        print("Quote:       " + str(q.citing_quote))
+
+        print("Citing Hash:  " + q.hash_citing())
+        print("Cited Hash:   " + str(q.hash_cited()))
+
+    url_results[row['CitingURL']] = q.hash_match()
 
 for url, url_match in url_results.items():
-    print(url_match, url)
+    print(str(url_match), ":", url)
+     
