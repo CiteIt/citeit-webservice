4dad1248f4f3070bce0a3c8e9e74b56a755209cb
Tim Langeman <timlangeman@gmail.com>
2019-12-20 07:25:19 -0500

    Update Test: call QuoteHastTest
      * loop through CSV and call URL()
      * call QuoteHashTest and print q.hashkey_diff()

--- app/test.py
+++ app/test.py
@@ -10,8 +10,15 @@
 # http://www.opensource.org/licenses/mit-license
 
 from lib.citeit_quote_context.tests.quote_hash_match import QuoteHashTest
+from lib.citeit_quote_context.url import URL
+from lib.citeit_quote_context.text_convert import TextConvert
+from lib.citeit_quote_context.text_convert import html_to_text
+from lib.citeit_quote_context.text_convert import levenshtein_distance
+from lib.citeit_quote_context.text_convert import show_diff
+
 import os
 import csv
+import requests
 
 
 __author__ = 'Tim Langeman'
@@ -20,58 +27,92 @@ __copyright__ = "Copyright (C) 2015-2019 Tim Langeman"
 __license__ = "MIT"
 __version__ = "0.3"
 
-debug = True
+debug = False
 
 filepath = "test_data/hashkeyTests - version3.csv"
 column_names = ['CitingURL', 'Quote', 'CitedURL', 'JavascriptHash', 'JavascriptHashKey']
 url_results = {}
+extra_chars = set()
 
 file = open(os.path.join(filepath), "r")
 reader = csv.DictReader(file, delimiter=',')
 
 for row_num, row in enumerate(reader):
-    #print("########################################################")
+
     for column_name in column_names:
         if debug:
             # print(column_name, " : " , row[column_name])
             pass
 
-    q = QuoteHashTest(
-        row['Quote'],         # excerpt from citing document
-        row['CitingURL'],     # url of the document that is doing the quoting
-        row['CitedURL'],      # url of document that is being quoted
-        row['JavascriptHash'],
-        row['JavascriptHashKey']
+    if (row_num < 36):
+        print("########################################################")
 
-    )
+        u = URL(row['CitingURL'])
+        citations = u.citations()
+        for quote in citations:
+            citing_quote = quote['citing_quote']
+            citing_quote_original = citing_quote
+            citing_quote = html_to_text(citing_quote)
+            citing_quote = TextConvert(citing_quote).escape()
 
-    if debug:
-        print(str(row_num) + "--------------------------------------------------------")
+        q = QuoteHashTest(
+            quote['citing_quote'], # excerpt from citing document
+            row['CitingURL'],      # url of the document that is doing the quoting
+            row['CitedURL'],       # url of document that is being quoted
+            row['JavascriptHash'],
+            row['JavascriptHashKey']
+        )
 
-        if q.hashkey_match():
-            # print("URL Match: " + str(q.citing_url))
-            pass
+        hashkey = citing_quote + "|" + row['CitingURL'] + "|" + row['CitedURL']
+        js_hashkey = row['JavascriptHashKey']
 
-        else:
-            print("########################################################")
-            print("HASH <>")
-            print("URL: " + str(q.citing_url))
-            print("Quote:       " + str(q.citing_quote))
+        print("URL:    " + str(q.citing_url))
+        print("Quote:  " + str(citing_quote_original))
+        print("Key:    " + citing_quote)
 
-            print("HASHKEY JS:     : " + str(q.hashkey_javascript))
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-            print("HASHKEY COMPUTED: " + str(q.hashkey_computed()))
-
-            print("JS Hash      :  " + str(q.hash_javascript))
-            print("Computed Hash:  " + str(q.hash_computed()))
-            print("########################################################")
-
-    url_results[row['CitingURL']] = q.hashkey_match() # q.hashkey_computed()  #
+        if (q.hashkey_diff_length() > 0):
+            print("JS:     " + str(q.hashkey_javascript))
+            print(">>      " + str(q.hashkey_computed()))
+            print("Diff >> " + str(q.hashkey_diff()))
 
 
+        """
+        if debug:
+            print("computed: " + str(q.hash_computed()))
+            print("js:       " + str(q.hash_javascript))
+            print(q.hashkey_computed())
+
+            if q.hashkey_match():
+                # print("URL Match: " + str(q.citing_url))
+                pass
+    
+            else:
+                print("########################################################")
+                print("HASH <>")
+                print("URL: " + str(q.citing_url))
+                print("Quote:       " + str(q.citing_quote))
+    
+                print("HASHKEY JS:     : " + str(q.hashkey_javascript))
+                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                print("HASHKEY COMPUTED: " + str(q.hashkey_computed()))
+    
+                print("JS Hash      :  " + str(q.hash_javascript))
+                print("Computed Hash:  " + str(q.hash_computed()))
+                print("########################################################")
+
+        url_results[row['CitingURL']] = quote_diff
+	"""	
+
+"""
+print("\n\n\n")
 print("# RESULTS:")
-for url, url_match in url_results.items():
-    if not url_match:
-        print(str(url_match), ":", url)
-        pass
-     
+
+for url, url_quote_diff in url_results.items():
+    if len(url_quote_diff) > 0:
+        print(str(url_quote_diff), ":", url)
+
+
+print('Escape these: ' + ''.join(extra_chars))
+for char in extra_chars:
+    print(char, " : " , ord(char), " : ",  char.encode('utf-8') )
+"""
