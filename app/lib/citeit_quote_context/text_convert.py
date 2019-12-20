86304192ab6f753bb33e6bf114ab880933f0e756
Tim Langeman <timlangeman@gmail.com>
2019-12-20 07:16:38 -0500

    Update TextConvert
      * add quotation marks and semicolon to escape characters
      * add levenshtein_distance
      * add show_diff which calculates character differences

--- app/lib/citeit_quote_context/text_convert.py
+++ app/lib/citeit_quote_context/text_convert.py
@@ -15,6 +15,7 @@ __version__ = "0.3"
 
 
 from bs4 import BeautifulSoup
+import difflib
 
 
 class TextConvert:
@@ -28,6 +29,7 @@ class TextConvert:
         txt = self.str
 
         replace_text = [' ', '\n', 'â€™', ',', '.', '-', '–','-','-', '—', ':', '/', '!', '`', '~', '^', "’"
+            , chr(34), chr(39), ";"
             , '&nbsp', '\xa0', '&#8217;'
             , '&#169;', '&copy;', '&#174;'
             , '&reg;', '&#8364;', '&euro;', '&#8482;', '&trade;'
@@ -42,6 +44,63 @@ class TextConvert:
             txt = txt.replace(t, '')
 
         return txt
+
+def levenshtein_distance(word1, word2):
+    """
+    https://en.wikipedia.org/wiki/Levenshtein_distance
+    :param word1:
+    :param word2:
+    :return:
+    """
+    word2 = word2.lower()
+    word1 = word1.lower()
+    matrix = [[0 for x in range(len(word2) + 1)] for x in range(len(word1) + 1)]
+
+    for x in range(len(word1) + 1):
+        matrix[x][0] = x
+    for y in range(len(word2) + 1):
+        matrix[0][y] = y
+
+    for x in range(1, len(word1) + 1):
+        for y in range(1, len(word2) + 1):
+            if word1[x - 1] == word2[y - 1]:
+                matrix[x][y] = min(
+                    matrix[x - 1][y] + 1,
+                    matrix[x - 1][y - 1],
+                    matrix[x][y - 1] + 1
+                )
+            else:
+                matrix[x][y] = min(
+                    matrix[x - 1][y] + 1,
+                    matrix[x - 1][y - 1] + 1,
+                    matrix[x][y - 1] + 1
+                )
+
+    return matrix[len(word1)][len(word2)]
+
+
+def show_diff(text, n_text):
+    """
+    http://stackoverflow.com/a/788780
+    Unify operations between two compared strings seqm is a difflib.
+    SequenceMatcher instance whose a & b are strings
+    """
+    seqm = difflib.SequenceMatcher(None, text, n_text)
+    output= []
+    for opcode, a0, a1, b0, b1 in seqm.get_opcodes():
+
+        if opcode == 'equal':
+            # output.append(seqm.a[a0:a1])
+            pass
+        elif opcode == 'insert':
+            output.append(seqm.b[b0:b1])
+        elif opcode == 'delete':
+            output.append(seqm.a[a0:a1])
+        elif opcode == 'replace':
+            output.append(seqm.b[b0:b1])
+
+    return ''.join(output)
+
 
 def html_to_text(html_str):
     soup = BeautifulSoup(html_str, 'html.parser')
