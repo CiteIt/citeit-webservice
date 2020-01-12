576da493f4a40fbf4871a7aaff017eaa1a8755da
Tim Langeman <timlangeman@gmail.com>
2020-01-11 19:30:45 -0500

    Add Unicode Escape Code Points to settings
      * Add Unicode Escape Code Points to settings

--- app/settings-default.py
+++ app/settings-default.py
@@ -9,3 +9,20 @@ VERSION_NUM = '0.4'  # used to version the sha256 hash filename
 
 JSON_FILE_PATH = '/tmp/'   # in Lambda, you need to save to /tmp folder
 SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost/database_name'
+
+
+
+# Remove the following Unicode code points from Hash
+URL_ESCAPE_CODE_POINTS = set ([
+    10, 20, 160
+])
+
+# Remove the following Unicode code points from Hash
+TEXT_ESCAPE_CODE_POINTS = set ( [
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18
+    , 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 39
+    , 96, 160, 173, 699, 700, 701, 702, 703, 712, 713, 714, 715, 716
+    , 717, 718, 719, 732, 733, 750, 757, 8211, 8212, 8213, 8216, 8217
+    , 8219, 8220, 8221, 8226, 8203, 8204, 8205, 65279, 8232, 8233, 133
+    , 5760, 6158, 8192, 8193, 8194, 8195, 8196, 8197, 8198, 8199, 8200
+    , 8201, 8202, 8239, 8287, 8288, 12288
