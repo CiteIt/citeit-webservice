3757e0bc7dda8bd14011026c9cdb9c4ac4bf892b
Tim Langeman <timlangeman@gmail.com>
2020-04-12 15:17:36 -0400

    Refactor settings.py for Docker os.getenv()
     * delete settings-default.py and settings-docker.py
     * add aws_settings_default.py

---
+++ app/aws_settings_default.py
@@ -0,0 +1,6 @@
+# Upload to AWS S3
+AMAZON_ACCESS_KEY = 'ABCDEFGH123456789'
+AMAZON_SECRET_KEY = 'alksdfj;2452lkjr;ajtsaljgfslakjfgassgf'
+AMAZON_S3_BUCKET = 'read.citeit.net'
+AMAZON_S3_ENDPOINT = 's3.amazonaws.com'
+AMAZON_REGION_NAME = 'us-east-1'
