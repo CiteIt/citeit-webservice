65b10b3aea778b3ff66f8202282915cc341e59d5
Tim Langeman <timlangeman@gmail.com>
2020-07-04 17:45:19 -0400

    add PDF > text conversion: generate from OCR
      * add PDF > text: generate from OCR
      * move doc_type to new Content_Type class
      * add pdf parsing libraries to requirements
      * add directories: downloads, text-versions

---
+++ app/lib/citeit_quote_context/content_type.py
@@ -0,0 +1,107 @@
+from urllib.parse import urlparse
+
+
+class Content_Type:
+
+    # Distinguish between html, text, .doc, ppt, and pdf
+    def doc_type(content_type):
+
+        if (content_type.startswith('text/html') or
+                content_type.startswith('application/html')
+        ):
+            doc_type = 'html'
+
+        elif content_type.startswith('application/pdf'):
+            doc_type = 'pdf'
+
+        elif ((content_type == 'text/plain') or
+              content_type.startswith('text')
+        ):
+            doc_type = 'txt'
+
+        elif content_type == 'application/rtf':
+            doc_type = 'rtf'
+
+        elif content_type == 'application/epub+zip':
+            doc_type = 'epub'
+
+        elif (content_type.startswith('application/msword') or
+              content_type.startswith(
+                  'application/vnd.openxmlformats-officedocument.wordprocessingml.document') or
+              content_type.startswith(
+                  'application/vnd.openxmlformats-officedocument.wordprocessingml.template') or
+              content_type.startswith(
+                  'application/vnd.ms-word.document.macroEnabled.12')
+        ):
+            doc_type = 'doc'
+
+        elif content_type.startswith('application/vnd.ms-powerpoint'):
+            doc_type = 'ppt'
+
+        elif content_type == 'audio/mpeg':
+            doc_type = 'audio/mpeg'
+
+        elif content_type == 'video/mpeg':
+            doc_type = 'video/mpeg'
+
+        elif content_type == 'audio/ogg':
+            doc_type = 'audio/ogg'
+
+        elif content_type == 'video/ogg':
+            doc_type = 'video/ogg'
+
+        elif content_type == 'application/ogg':
+            doc_type = 'audio/ogg'
+
+        elif (content_type == 'audio/wav'):
+            doc_type = 'wav'
+
+        elif (content_type == 'audio/aac'):
+            doc_type = 'aac'
+
+        elif (content_type == 'image/jpg' or
+              content_type == 'image/jpeg'
+        ):
+            doc_type = 'jpg'
+
+        elif content_type == 'image/png':
+            doc_type = 'png'
+
+        elif content_type == 'image/tiff':
+            doc_type = 'tiff'
+
+        elif content_type == 'image/webp':
+            doc_type = 'webp'
+
+        elif content_type == 'image/gif':
+            doc_type = 'gif'
+
+        elif content_type == 'application/vnd.ms-excel':
+            doc_type = 'xls'
+
+        elif ((
+                      content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') or
+              (
+                      content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.template')
+        ):
+            doc_type = 'xlsx'
+
+        else:
+            doc_type = 'error: no doc_type: ' + content_type
+
+        return doc_type
+
+
+    def media_provider(domain):
+        domain = urlparse('http://www.example.test/foo/bar').netloc
+
+        if domain == 'www.youtube.com':
+            domain = 'youtube'
+
+        elif domain == 'vimeo.com':
+            domain = 'vimeo'
+
+        elif domain == 'soundcloud.com':
+            domain = 'soundcloud'
+
+        return domain
--- app/lib/citeit_quote_context/document.py
+++ app/lib/citeit_quote_context/document.py
@@ -10,12 +10,20 @@
 from lib.citeit_quote_context.canonical_url import Canonical_URL
 from bs4 import BeautifulSoup
 from functools import lru_cache
-import certifi
+
 import requests
+import urllib
+import pdftotext      # pdf that have digital text: easier to convert than ocr
+# import pytesseract  # ocr library for python
+# from pdf2image import convert_from_path
+# import glob
+# import timeit
+
 from datetime import datetime
 from langdetect import detect    # https://www.geeksforgeeks.org/detect-an-unknown-language-using-python/
 import ftfy     # Fix bad unicode:  http://ftfy.readthedocs.io/
 import re
+import os
 
 
 
@@ -42,7 +50,7 @@ class Document:
         self.request_stop = None  # Datetime of last download
         self.char_encoding = ''   # character encoding of document, returned by requests library
 
-    # def url(self):
+    #def url(self):
     #    return self.url
 
     def download_resource(self):
@@ -65,7 +73,7 @@ class Document:
             encoding = r.encoding
             language = detect(r.text)  # https://www.geeksforgeeks.org/detect-an-unknown-language-using-python/
             error = ''
-
+            content_type = r.headers['Content-Type']
 
         except requests.HTTPError:
             self.request_stop = datetime.now()
@@ -78,7 +86,8 @@ class Document:
                     'content': content,   # raw
                     'encoding': encoding,
                     'error': error,
-                    'language': language
+                    'language': language,
+                    'content_type': content_type
                 }
 
     @lru_cache(maxsize=20)
@@ -101,7 +110,9 @@ class Document:
             Idea: https://github.com/skylander86/lambda-text-extractor
         """
 
-        if self.doc_type() == 'html':
+        doc_type = self.doc_type()
+
+        if (doc_type == 'html'):
             soup = BeautifulSoup(self.html(), "html.parser")
             invisible_tags = ['style', 'script', '[document]', 'head', 'title']
             for elem in soup.findAll(invisible_tags):
@@ -110,28 +121,229 @@ class Document:
             text = ftfy.fix_text(text)  # fix unicode problems
             text = convert_quotes_to_straight(text)
             text = normalize_whitespace(text)
+
             return text
 
-        elif self.doc_type == 'pdf':
-            # https://github.com/euske/pdfminer/
-            return "not implemented"
+        elif (doc_type == 'pdf'):
+            # example: https://demo.citeit.net/2020/06/30/well-behaved-women-seldom-make-history-original-pdf/
 
-        elif self.doc_type == 'doc':
-            # https://github.com/deanmalmgren/textract
-            return "not implemented"
+            text_filename = urllib.parse.quote_plus(self.url)
+            headers = {'user-agent': 'Mozilla / 5.0(Windows NT 6.1;'
+                                     ' WOW64; rv: 54.0) Gecko/20100101 Firefox/71.0'}
 
-        elif self.doc_type == 'text':
+            # Save PDF to temp file
+            r = requests.get(self.url, headers=headers, verify=False)
+            open("../downloads/"+ text_filename, 'wb').write(r.content)
+            print("Saving text to filename: " + text_filename)
+
+
+            # Extract text from original pdf file
+            with open("../downloads/" + text_filename, 'rb') as f:
+                pdf = pdftotext.PDF(f)
+
+            """
+            // Credit: https://pypi.org/project/pdftotext/
+            
+            # How many pages?
+            print(len(pdf))
+
+            # Iterate over all the pages
+            for page in pdf:
+                print(page)
+
+            # Read some individual pages
+            print(pdf[0])
+            print(pdf[1])
+            """
+
+            pdf_text = "\n\n".join(pdf)  # Combine text into single string
+            pdf_text = pdf_text.strip()
+
+            # If able to generate text version from digital PDF
+            if (len(pdf_text) > 0):
+
+                # Write text version to file:
+                text_version_filename = "../text-versions/" +  \
+                    os.path.splitext(text_filename)[0] + '.txt'  # replace .pdf file extension
+
+
+                print("Saving text version to: " + text_version_filename)
+                open(text_version_filename, 'w').write(pdf_text)
+
+                return pdf_text
+
+            else: # Generate text version from scanned doc using OCR (more CPU intensive)
+
+                """
+                language = 'eng'  # hard coded for now
+                input_filename = r"Philosophy-of-Hypertext.pdf"
+                pdfs = glob.glob(text_filename)
+                output = ''
+    
+                for pdf_path in pdfs:
+                    pages = convert_from_path(pdf_path, 500)
+    
+                    for pageNum, imgBlob in enumerate(pages):
+                        text = pytesseract.image_to_string(imgBlob, lang=language)
+                        output_filename = output_file_base + '_page_' + str(pageNum) + '.txt'
+
+                        with open(output_filename, 'w') as the_file:
+                            #the_file.write(text)
+                            output = output.append(text)
+                """
+
+                error_text = "PDF file identified but unable to extract text. OCR Scanning not yet implemented."
+
+                return error_text
+
+        elif (doc_type == 'txt'):
             return self.raw()
 
-        return 'error: no doc_type'
+        elif (doc_type == 'rtf'):
+            return "RTF support not yet implemented.  To implement it see: http://www.gnu.org/software/unrtf/"
+
+        elif (doc_type == 'epub'):
+            return "EPub support not yet implemented.  To implement it see: https://github.com/aerkalov/ebooklib"
+
+        elif (doc_type == 'doc'):
+            return "Doc support not yet implemented. To implement it see: http://www.winfield.demon.nl/"
+
+        elif (doc_type == 'docx'):
+            return "Doc support not yet implemented. To implement it see: https://github.com/ankushshah89/python-docx2txt"
+
+        elif (doc_type == 'pptx'):
+            return "Powerpoint support not yet implemented.  To implement it see:  https://python-pptx.readthedocs.org/en/latest/"
+
+        elif (doc_type == 'ps'):
+            return "Doc support not yet implemented.  To implement it see: http://www.winfield.demon.nl/"
+
+        elif (doc_type == 'audio/mpeg'):
+            return "Doc support not yet implemented.  To implement it see: http://www.winfield.demon.nl/"
+
+        elif (doc_type == 'audio/mpeg'):
+            return "MP3 support not yet implemented.  To implement it see: https://pypi.org/project/SpeechRecognition/"
+
+        elif (doc_type == 'audio/ogg'):
+            return "OGG support not yet implemented.  To implement it see: https://pypi.org/project/SpeechRecognition/"
+
+        elif (doc_type == 'video/ogg'):
+            return "OGG support not yet implemented.  To implement it see: https://pypi.org/project/SpeechRecognition/"
+
+        elif (doc_type == 'wav'):
+            return "WAV support not yet implemented.  To implement it see: https://pypi.org/project/SpeechRecognition/"
+
+        elif (doc_type == 'aac'):
+            return "AAC support not yet implemented.  To implement it see: https://pypi.org/project/SpeechRecognition/"
+
+        elif (doc_type == 'jpg'):
+            return "JPG support not yet implemented.  To implement it see: https://github.com/tesseract-ocr/"
+
+        elif (doc_type == 'png'):
+            return "PNG support not yet implemented.  To implement it see: https://github.com/tesseract-ocr/"
+
+        elif (doc_type == 'tiff'):
+            return "TIFF support not yet implemented.  To implement it see: https://github.com/tesseract-ocr/"
+
+        elif (doc_type == 'gif'):
+            return "GIF support not yet implemented.  To implement it see: https://github.com/tesseract-ocr/"
+
+        elif (doc_type == 'webp'):
+            return "WebP support not yet implemented. Does Tesseract support it?  https://github.com/tesseract-ocr/"
+
+        elif (doc_type == 'xls'):
+            return "XLS support not yet implemented.  To implement it see: https://pypi.python.org/pypi/xlrd"
+
+        elif (doc_type == 'xlsx'):
+            return "XLSX support not yet implemented.  To implement it see: https://pypi.python.org/pypi/xlrd"
+
+        else:
+            return 'error: no doc_type: ' + doc_type
 
     def doc_type(self):
-        """ TODO: Distinguish between html, text, .doc, and pdf"""
-        # mime = magic.Magic(mime=True)
-        # doc_type = mime.from_file(self.raw())
-        # import magic	# https://github.com/ahupp/python-magic
-        # return doc_type
-        return 'html'  # hardcode to html for now
+        # Distinguish between html, text, .doc, ppt, and pdf
+        content_type = self.download_resource()['content_type']
+
+        if  (   content_type.startswith('text/html') or
+                content_type.startswith('application/html')
+            ):
+            doc_type = 'html'
+
+        elif    content_type.startswith('application/pdf'):
+            doc_type = 'pdf'
+
+        elif (  (content_type == 'text/plain') or
+                content_type.startswith('text')
+            ):
+            doc_type = 'txt'
+
+        elif    content_type == 'application/rtf':
+            doc_type = 'rtf'
+
+        elif    content_type == 'application/epub+zip':
+            doc_type = 'epub'
+
+        elif (  content_type.startswith('application/msword') or
+                content_type.startswith('application/vnd.openxmlformats-officedocument.wordprocessingml.document') or
+                content_type.startswith('application/vnd.openxmlformats-officedocument.wordprocessingml.template') or
+                content_type.startswith('application/vnd.ms-word.document.macroEnabled.12')
+            ):
+            doc_type = 'doc'
+
+        elif    content_type.startswith('application/vnd.ms-powerpoint'):
+            doc_type = 'ppt'
+
+        elif    content_type == 'audio/mpeg':
+            doc_type = 'audio/mpeg'
+
+        elif    content_type == 'video/mpeg':
+            doc_type = 'video/mpeg'
+
+        elif    content_type == 'audio/ogg':
+            doc_type = 'audio/ogg'
+
+        elif    content_type == 'video/ogg':
+            doc_type = 'video/ogg'
+
+        elif    content_type == 'application/ogg':
+            doc_type = 'audio/ogg'
+
+        elif( content_type == 'audio/wav'):
+            doc_type = 'wav'
+
+        elif (content_type == 'audio/aac'):
+            doc_type = 'aac'
+
+        elif (  content_type == 'image/jpg' or
+                content_type == 'image/jpeg'
+            ):
+            doc_type = 'jpg'
+
+        elif    content_type == 'image/png':
+            doc_type = 'png'
+
+        elif    content_type == 'image/tiff':
+            doc_type = 'tiff'
+
+        elif    content_type == 'image/webp':
+            doc_type = 'webp'
+
+        elif    content_type == 'image/gif':
+            doc_type = 'gif'
+
+        elif    content_type == 'application/vnd.ms-excel':
+            doc_type = 'xls'
+
+        elif (  (content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') or
+                (content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.template')
+            );
+            doc_type = 'xlsx'
+
+        else:
+             doc_type = 'error: no doc_type: ' + doc_type
+
+        print("Doc Type: " + doc_type)
+        return doc_type
+
 
     @lru_cache(maxsize=20)
     def raw(self, convert_to_unicode=True):
